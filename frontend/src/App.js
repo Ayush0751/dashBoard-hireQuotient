f7a32d6582c33a5d3cf18c2e6cc41968a995cb8b
Ayush Raj <76896862+Ayush0751@users.noreply.github.com>
2023-12-03 23:26:11 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainTable from "../src/Components/MainTable"
+
+function App() {
+  return (
+    <div>
+      <MainTable/>
+    </div>
+  );
+}
+
+export default App;
